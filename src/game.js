d28d4d490a253ac4f9733607d982e8753cc0cf35
Adam Kidder <thekidder@gmail.com>
2013-12-14 12:18:34 -0800

    playing with tooltips

--- src/game.js
+++ src/game.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   noise.seed(Math.random());
 
   Crafty.init(900,600, document.getElementById('game'));
-  Crafty.e('2D, Canvas, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
+
+  $.get("html/tooltip.html", function(data) {
+    Crafty.e('2D, DOM, HTML').attr({x: 0, y: 0}).replace(data);
+  });
 });
